fbc63279e06ca4fd25b00dd898aad0fc15692a35
eladshamailov <eladsham@post.bgu.ac.il>
2017-11-24 19:30:17 +0200

    before changes on workers

--- src/main/java/NamedEntityRecognition.java
+++ src/main/java/NamedEntityRecognition.java
@@ -51,29 +51,4 @@ public class NamedEntityRecognition {
         }
         return arrayList;
     }
-    public static void printEnt(String review){
-        // create an empty Annotation just with the given text
-        Annotation document = new Annotation(review);
-
-        // run all Annotators on this text
-        NERPipeline.annotate(document);
-
-        // these are all the sentences in this document
-        // a CoreMap is essentially a Map that uses class objects as keys and has values with custom types
-        List<CoreMap> sentences = document.get(SentencesAnnotation.class);
-
-        for(CoreMap sentence: sentences) {
-            // traversing the words in the current sentence
-            // a CoreLabel is a CoreMap with additional token-specific methods
-            for (CoreLabel token: sentence.get(TokensAnnotation.class)) {
-                // this is the text of the token
-                String word = token.get(TextAnnotation.class);
-                // this is the NER label of the token
-                String ne = token.get(NamedEntityTagAnnotation.class);
-                System.out.println("\t-" + word + ":" + ne);
-            }
-        }
-
-    }
-
 }
